7756bd696b5c2144599ff3178cf3e246e6140e9f
Josh Holtrop <jholtrop+git@gmail.com>
2010-05-03 16:22:55 -0400

    added initial tmpl files

---
+++ tmpl/parser.cc
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+#include I_HEADER_NAME
+
+using namespace std;
+
+#ifdef I_NAMESPACE
+namespace I_NAMESPACE {
+#endif
+
+void parse(istream & i)
+{
+}
+
+#ifdef I_NAMESPACE
+};
+#endif
---
+++ tmpl/parser.h
@@ -0,0 +1,23 @@
+
+#ifndef IMBECILE_PARSER_HEADER
+#define IMBECILE_PARSER_HEADER
+
+#include <iostream>
+
+#ifdef I_NAMESPACE
+namespace I_NAMESPACE {
+#endif
+
+class I_CLASSNAME
+{
+    public:
+        void parse(std::istream & in);
+
+    protected:
+};
+
+#ifdef I_NAMESPACE
+};
+#endif
+
+#endif /* IMBECILE_PARSER_HEADER */
