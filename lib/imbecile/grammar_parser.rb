23b7782a5ddd436364d88d5bab191f5afe6c0cba
Josh Holtrop <jholtrop@gmail.com>
2021-05-01 09:31:12 -0400

    Begin parsing grammar

--- lib/imbecile/grammar_parser.rb
+++ lib/imbecile/grammar_parser.rb
@@ -2,6 +2,16 @@ module Imbecile
   class GrammarParser
 
     def initialize(input_file)
+      File.read(input_file).each_line.each_with_index do |line, line_index|
+        line = line.chomp
+        line_number = line_index + 1
+        if line =~ /^\s*token\s+(\S+)\s+(.*)$/
+          name, expr = $1, $2
+          unless name =~ /^[a-zA-Z_][a-zA-Z_0-9]*$/
+            raise "Invalid token name #{name} on line #{line_number}"
+          end
+        end
+      end
     end
 
   end
