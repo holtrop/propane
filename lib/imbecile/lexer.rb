f295acb5932633dff6626bd8478b8bec9c559f2e
Josh Holtrop <jholtrop@gmail.com>
2021-08-19 13:11:12 -0400

    Generator builds a Lexer, not a Lexer::DFA

--- lib/imbecile/lexer.rb
+++ lib/imbecile/lexer.rb
@@ -1,5 +1,13 @@
 module Imbecile
   class Lexer
 
+    # @return [DFA]
+    #   Lexer DFA.
+    attr_accessor :dfa
+
+    def initialize(grammar)
+      @dfa = DFA.new(grammar.tokens)
+    end
+
   end
 end
