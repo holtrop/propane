9273bfccf62141f0befdf709bcc9d86f6fc68a43
Josh Holtrop <jholtrop@gmail.com>
2021-08-19 20:00:40 -0400

    Move Token/Rule out of Grammar class

---
+++ lib/imbecile/rule.rb
@@ -0,0 +1,19 @@
+module Imbecile
+
+  class Rule
+
+    attr_reader :name
+
+    attr_reader :components
+
+    attr_reader :code
+
+    def initialize(name, rule_components, code)
+      @name = name
+      @components = rule_components
+      @code = code
+    end
+
+  end
+
+end
