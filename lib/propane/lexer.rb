6f1ce32775bd239dcffed12164788fd68893838f
Josh Holtrop <jholtrop@gmail.com>
2022-06-05 14:18:35 -0400

    Move Lexer::DFA#build_tables to Lexer

--- lib/propane/lexer.rb
+++ lib/propane/lexer.rb
@@ -9,5 +9,34 @@ class Propane
       @dfa = DFA.new(tokens)
     end
 
+    def build_tables
+      transition_table = []
+      state_table = []
+      states = @dfa.enumerate
+      states.each do |state, id|
+        accepts =
+          if state.accepts.nil?
+            TOKEN_NONE
+          elsif state.accepts.name
+            state.accepts.id
+          else
+            TOKEN_DROP
+          end
+        state_table << {
+          transition_table_index: transition_table.size,
+          n_transitions: state.transitions.size,
+          accepts: accepts,
+        }
+        state.transitions.each do |transition|
+          transition_table << {
+            first: transition.code_point_range.first,
+            last: transition.code_point_range.last,
+            destination: states[transition.destination],
+          }
+        end
+      end
+      [transition_table, state_table]
+    end
+
   end
 end
--- lib/propane/lexer/dfa.rb
+++ lib/propane/lexer/dfa.rb
@@ -22,35 +22,6 @@ class Propane
         @start_state = @states[0]
       end
 
-      def build_tables
-        transition_table = []
-        state_table = []
-        states = enumerate
-        states.each do |state, id|
-          accepts =
-            if state.accepts.nil?
-              TOKEN_NONE
-            elsif state.accepts.name
-              state.accepts.id
-            else
-              TOKEN_DROP
-            end
-          state_table << {
-            transition_table_index: transition_table.size,
-            n_transitions: state.transitions.size,
-            accepts: accepts,
-          }
-          state.transitions.each do |transition|
-            transition_table << {
-              first: transition.code_point_range.first,
-              last: transition.code_point_range.last,
-              destination: states[transition.destination],
-            }
-          end
-        end
-        [transition_table, state_table]
-      end
-
       private
 
       def register_nfa_state_set(nfa_state_set)
