0f1c00d1a6ae0f1c360d52d9803a9fc067768d2b
Josh Holtrop <jholtrop@gmail.com>
2022-06-14 07:35:58 -0400

    Add RuleSet#could_be_empty?

--- lib/propane/rule_set.rb
+++ lib/propane/rule_set.rb
@@ -2,17 +2,41 @@ class Propane
 
   class RuleSet
 
+    # @return [String]
+    #   Name of the RuleSet.
     attr_reader :name
 
+    # @return [Array<Rule>]
+    #   Rules in the RuleSet.
     attr_reader :rules
 
+    # Construct a RuleSet.
+    #
+    # @param name [String]
+    #   Name of the RuleSet.
     def initialize(name)
       @name = name
       @rules = []
+      @could_be_empty = false
     end
 
+    # Add a Rule to the RuleSet.
+    #
+    # @param rule [Rule]
+    #   Rule to add.
     def <<(rule)
       @rules << rule
+      if rule.empty?
+        @could_be_empty = true
+      end
+    end
+
+    # Return whether any Rule in the RuleSet is empty.
+    #
+    # @return [Boolean]
+    #   Whether any rule in the RuleSet is empty.
+    def could_be_empty?
+      @could_be_empty
     end
 
   end
