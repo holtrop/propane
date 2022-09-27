a4c55468761c5dc391d34195f6560305fa899d41
Josh Holtrop <jholtrop@gmail.com>
2022-09-27 12:33:23 -0400

    Disable parser debug output

--- spec/test_user_code.d
+++ spec/test_user_code.d
@@ -11,10 +11,8 @@ unittest
     string input = "abcdef";
     auto parser = new Testparser.Parser(cast(const(ubyte) *)input.ptr, input.length);
     assert(parser.parse() == true);
-    writeln("pass1");
 
     input = "abcabcdef";
     parser = new Testparser.Parser(cast(const(ubyte) *)input.ptr, input.length);
     assert(parser.parse() == true);
-    writeln("pass2");
 }
