f902c835c9a3aebc8822269b95e7e83fd3b5598e
Josh Holtrop <jholtrop@gmail.com>
2026-08-11 23:48:07 -0400

    Fix Rust prefixed status code values

--- spec/test_multiple_parsers.rs
+++ spec/test_multiple_parsers.rs
@@ -3,10 +3,10 @@ use testparsermyp2 as m2;
 
 fn main() {
     let mut context1 = m1::myp1_context_new(b"a\n1");
-    assert_eq!(m1::P_SUCCESS, m1::myp1_parse(&mut context1));
+    assert_eq!(m1::MYP1_SUCCESS, m1::myp1_parse(&mut context1));
     m1::myp1_context_delete(context1);
 
     let mut context2 = m2::myp2_context_new(b"bcb");
-    assert_eq!(m2::P_SUCCESS, m2::myp2_parse(&mut context2));
+    assert_eq!(m2::MYP2_SUCCESS, m2::myp2_parse(&mut context2));
     m2::myp2_context_delete(context2);
 }
